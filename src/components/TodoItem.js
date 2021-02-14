9c5d3d8484b38fe58d0f3fe1e43276024ff27e7f
Zara <zara@gene.co.uk>
2021-02-14 19:52:56 +0100

    can edit todoitems and resave

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,6 +4,22 @@ import styles from "./TodoItem.module.css"
 
 class TodoItem extends React.Component {
 
+  state = {
+    editing: false,
+  }
+
+  handleEditing = () => {
+    this.setState({
+      editing: true,
+    })
+  }
+
+  handleUpdateDone = event => {
+    if (event.key === "Enter") {
+      this.setState({ editing: false })
+    }
+  }
+
   render() {
 
     const completedStyle = {
@@ -15,20 +31,41 @@ class TodoItem extends React.Component {
 
     const { completed, id, title } = this.props.todo
 
+    let viewMode = {}
+    let editMode = {}
+
+    if (this.state.editing) {
+      viewMode.display = "none"
+    } else {
+      editMode.display = "none"
+    }
+
     return (
       <li className={styles.item}>
+        <div onDoubleClick={this.handleEditing} style={viewMode}>
+          <input 
+            type="checkbox"
+            className={styles.checkbox} 
+            checked={completed}
+            onChange={() => this.props.handleChangeProps(id)}
+          />
+          <button onClick={() => this.props.deleteTodoProps(id)}>
+            Delete
+          </button>
+          <span style={completed ? completedStyle : null}>
+            {title}
+          </span>
+        </div>
         <input 
-          type="checkbox"
-          className={styles.checkbox} 
-          checked={completed}
-          onChange={() => this.props.handleChangeProps(id)}
+          type="text" 
+          className={styles.textInput}
+          value={title}
+          style={editMode}
+          onChange={e => {
+            this.props.setUpdate(e.target.value, id)
+          }}
+          onKeyDown={this.handleUpdateDone}
         />
-        <button onClick={() => this.props.deleteTodoProps(id)}>
-          Delete
-        </button>
-        <span style={completed ? completedStyle : null}>
-          {title}
-        </span>
       </li>
     )
     
