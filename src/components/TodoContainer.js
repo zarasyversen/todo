425804ded0968851d6f74a71714bd0726ab7cecd
Zara <zara@gene.co.uk>
2021-02-21 13:35:31 +0100

    add useEffect and load inital todos

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { v4 as uuidv4 } from "uuid";
 import Header from "./Header"
 import InputTodo from "./InputTodo"
 import TodosList from "./TodosList"
 
 const TodoContainer = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(getInitialTodos());
 
   const handleChange = id => {
     setTodos(prevState =>
@@ -48,6 +48,17 @@ const TodoContainer = () => {
       })
     )
   }
+
+  function getInitialTodos() {
+    const temp = localStorage.getItem("todos")
+    const savedTodos = JSON.parse(temp)
+    return savedTodos || []
+  }
+
+  useEffect(() => {
+    const temp = JSON.stringify(todos)
+    localStorage.setItem("todos", temp)
+  }, [todos])
 
   return (
     <div className="container">
