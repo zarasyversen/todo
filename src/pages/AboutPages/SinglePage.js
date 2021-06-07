f783961b0bb8821e9230bec3fcc08f9ed510280e
Zara <zara.syversen@askas.se>
2021-06-07 18:00:26 +0200

    more info, update favicon

--- src/pages/AboutPages/SinglePage.js
+++ src/pages/AboutPages/SinglePage.js
@@ -17,21 +17,21 @@ const SinglePages = () => {
           <p>
             This app will persist your data in the browser local storage. So
             whether you reload, close your app or reopened it, you still have
-            access to your to-dos items.
+            access to your to-do items.
           </p>
         </section>
         <section className="section">
         <h2 className="sectionHeading">How was this app built?</h2>
-        <p>This project was originally based off a really great tutorial by @ibaslogic.</p>
+          <p>This project was originally based off a really great tutorial by <a href="https://ibaslogic.com/" target="_blank" rel="noopener">ibaslogic.</a></p>
           <p>I have amended and added quite a few things:</p>
           <ul>
-            <li>Rewrote adding todo to be visible only on "Add Task" click</li>
+            <li>Rewrote adding todo to be visible only when you click a button "Add Task".</li>
             <li>
               Added an edit button (previously the todo was editable on double
-              click
+              click) and automatically focus on input.
             </li>
             <li>Added ability to search and show filtered results</li>
-            <li>Enhanced accessiblity and clearer UX</li>
+            <li>Improved accessiblity</li>
             <li>Added counter</li>
             <li>Added time and day for each task</li>
             <li>Add logic to handle what the app looks like with no items</li>
@@ -46,10 +46,13 @@ const SinglePages = () => {
       slug: "about-author",
       title: "About the Author",
       description: (
-        <p>
-          I'm Zara and I love learning. Check out some more projects on my
-          Github
-        </p>
+        <div className="section">
+          <p>
+            I'm Zara and I love learning. Check out some more projects on <a href="https://github.com/zarasyversen" target="_blank" rel="noopener">my
+            Github</a> or <a href="https://twitter.com/madebyzara" target="_blank" rel="noopener">follow me on Twitter.</a>
+          </p>
+        </div>
+   
       ),
     },
   ];
--- src/pages/AboutPages/Start.js
+++ src/pages/AboutPages/Start.js
@@ -5,7 +5,10 @@ const Start = () => {
   return (
     <main>
       <h1>About</h1>
-      <p>Here you can find some more information about this app and it's creator, but the main purpose of this page is created to learn routes, sub-pages and how to render different content based on slugs (about/about-author).</p>
+      <section className="section">
+        <h2 className="sectionHeading">Learning new things</h2>
+        <p>The main purpose of this page is created to learn routes, sub-pages and how to render different content based on slugs (about/about-author). Check out some of the subpages!</p>
+      </section>
     </main>
   );
 };
